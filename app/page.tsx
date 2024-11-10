fd7033e5222f2e8a22032c795b2ef241de583a1c
nizam323 <muhammadnizamuddin.sce@gmail.com>
2024-11-10 23:19:40 +0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <>
+      <center style={{backgroundColor:"tomato",display:"flex",alignItems:"center",justifyContent:"center",height:"100vh",flexDirection:"column"}}>
+        <h1 style={{color:"wheat"}}>hello world</h1>
+        <h1 style={{color:"wheat"}}>in next js</h1>
+      </center>
+    </>
+  );
+}
