0a90cfbf087e97b7ecb3a86cead483220503a9d6
nizam323 <muhammadnizamuddin.sce@gmail.com>
2024-11-10 23:55:44 +0500

    complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
+import Con from './home/page'
+
 export default function Home() {
   return (
     <>
-      <center style={{backgroundColor:"tomato",display:"flex",alignItems:"center",justifyContent:"center",height:"100vh",flexDirection:"column"}}>
-        <h1 style={{color:"wheat"}}>hello world</h1>
-        <h1 style={{color:"wheat"}}>in next js</h1>
-      </center>
+    <Con />
     </>
   );
 }
